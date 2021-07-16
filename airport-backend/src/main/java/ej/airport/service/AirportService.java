0636bd936c0cf6bdf93dd5481d728e1a10bb7793
Evaldas Jurgaitis <ejurgaitis@baltic-amadeus.lt>
2021-07-16 12:28:56 +0300

    TASK5 - feign client impl -- fetch airport price from airport price service

--- airport-backend/src/main/java/ej/airport/service/AirportService.java
+++ airport-backend/src/main/java/ej/airport/service/AirportService.java
@@ -1,12 +1,11 @@
 package ej.airport.service;
 
 import ej.airport.entity.Airport;
+import ej.airport.feign.AirportPriceFeignClient;
 import ej.airport.repository.AirportRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class AirportService {
@@ -20,14 +19,6 @@ public class AirportService {
     public Airport getAirport(Long id) {
         return airportRepository.getById(id);
     }
-
-//    public List<Airport> getAirportsByIsoCountry(String isoCountry) {
-//        return airportRepository.findByIsoCountry(isoCountry);
-//    }
-//
-//    public List<Airport> getAirportsByIsoCountryAndIsoRegion(String isoCountry, String isoRegion) {
-//        return airportRepository.findByIsoCountryAndIsoRegion(isoCountry, isoRegion);
-//    }
 
     public Page<Airport> getAirportsByIsoCountry(String isoCountry, Pageable pageable) {
         return airportRepository.findByIsoCountry(isoCountry, pageable);
