e44797ebe52d18957c630c5a6cac9cf204039568
Evaldas Jurgaitis <ejurgaitis@baltic-amadeus.lt>
2021-12-14 18:58:17 +0200

    Add airport customer service module nad rename old modules

---
+++ airport-customer-service/src/main/java/ej/airport/AirportCustomerServiceApplication.java
@@ -0,0 +1,13 @@
+package ej.airport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportCustomerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirportCustomerServiceApplication.class, args);
+    }
+
+}
